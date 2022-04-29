259faa52957b3e5e22848165a751383ce682f6a0
CoderChris210 <93211826+CoderChris210@users.noreply.github.com>
2022-04-28 22:55:54 -0700

    Update script.js

--- script.js
+++ script.js
@@ -7,19 +7,25 @@ const switchinnerHTML = (modinnerhtmlval) => {
   resulttext.innerHTML = modinnerhtmlval
 }
 const choices = {
-    rock_choice : {
-      name: 'Rock',
-      value: 0
-    },
-    scissors_choice: {
-      name: 'Scissors',
-      value: 1
-    },
-    paper_choice: {
-      name: 'Paper',
-      value: 2
-    }
+  rock_choice : {
+    name: 'Rock',
+    name_console: 'You',
+    name_console_op: 'The bot',
+    value: 0
+  },
+  scissors_choice: {
+    name: 'Scissors',
+    name_console: 'You',
+    name_console_op: 'The bot',
+    value: 1
+  },
+  paper_choice: {
+    name: 'Paper',
+    name_console: 'You',
+    name_console_op: 'The bot',
+    value: 2
   }
+}
   let ai_choice = Math.floor(Math.random() * 3)
   const decideaichoice = () => {
     if (ai_choice === choices.rock_choice.value) {
@@ -40,29 +46,30 @@ const choices = {
   }
   decideaichoice()
   winner = null;
+
   const play = (pl1_choice, pl2_choice) => {
     if (pl1_choice === choices.rock_choice && pl2_choice === choices.scissors_choice) {
-      winner = pl1_choice.name
+      winner = pl1_choice.name_console
       win_message(winner)
     }
     else if (pl1_choice === choices.rock_choice && pl2_choice === choices.paper_choice) {
-      winner = pl2_choice.name
+      winner = pl2_choice.name_console_op
       win_message(winner)
     }
     else if(pl1_choice === choices.scissors_choice && pl2_choice === choices.paper_choice) {
-      winner = pl1_choice.name
+      winner = pl1_choice.name_console
       win_message(winner)
     }
     else if(pl1_choice === choices.scissors_choice && pl2_choice === choices.rock_choice) {
-      winner = pl2_choice.name
+      winner = pl2_choice.name_console_op
       win_message(winner)
     }
     else if(pl1_choice === choices.paper_choice && pl2_choice === choices.rock_choice) {
-      winner = pl1_choice.name
+      winner = pl1_choice.name_console
       win_message(winner)
     }
     else if(pl1_choice === choices.paper_choice && pl2_choice === choices.scissors_choice) {
-      winner = pl2_choice.name
+      winner = pl2_choice.name_console_op
       win_message(winner)
     }
     else if(pl1_choice === pl2_choice) {
