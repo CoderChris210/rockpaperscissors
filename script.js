65781d1009bff853d038fed7aee51f92a3f73d4f
CoderChris210 <93211826+CoderChris210@users.noreply.github.com>
2022-05-09 22:23:50 -0700

    fixed logic

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
+const aichoicetext = document.getElementById('aichoicetext')
 const resulttext = document.getElementById('resulttext')
+const humanchoicetext = document.getElementById('humanchoicetext')
 const rockbtn = document.getElementById('rockbtn')
 const paperbtn = document.getElementById('paperbtn')
 const scissorsbtn = document.getElementById('scissorsbtn')
 const restartbtn = document.getElementById('restartbtn')
-const humanchoicetext = document.getElementById('humanchoicetext')
-const aichoicetext = document.getElementById('aichoicetext')
 const switchinnerHTML = (modinnerhtmlval) => {
   resulttext.innerHTML = modinnerhtmlval
 }
@@ -28,7 +28,7 @@ const choices = {
     value: 2
   }
 }
-let ai_choice = Math.floor(Math.random() * 2)
+let ai_choice = Math.floor(Math.random() * 3)
   const decideaichoice = () => {
     if (ai_choice === choices.rock_choice.value) {
       ai_choice = choices.rock_choice
@@ -40,9 +40,13 @@ let ai_choice = Math.floor(Math.random() * 2)
       ai_choice = choices.paper_choice
     }
     else {
-      alert('AI = NULL: FATAL ERROR IN decideaichoice()')
+      alert('FATAL ERROR IN decideaichoice()')
     }
   }
+const rotateai_choice = () => {
+  const rotatenumber = Math.floor(Math.random() * 3);
+  ai_choice = rotatenumber;
+}
   const win_message = (winner) => {
     switchinnerHTML(`<h1>The winner is... \n${winner.toUpperCase()}!</h1>`)
   }
@@ -50,13 +54,6 @@ let ai_choice = Math.floor(Math.random() * 2)
     humanchoicetext.innerHTML = `You chose: ${pl1.name}`
     aichoicetext.innerHTML = `The bot chose: ${pl2.name}`
 }
-const call_player_choices = (pl1_choice, pl2_choice) => {
-    player_choices(pl1_choice, pl2_choice)
-}
-  const rotateai_choice = () => {
-    const rotatenumber = Math.floor(Math.random() * 2);
-    ai_choice = rotatenumber;
-  }
   decideaichoice()
   winner = null;
 
@@ -92,29 +89,39 @@ const call_player_choices = (pl1_choice, pl2_choice) => {
     else {
       alert('FATAL ERROR: play()')
     }
+    console.log(pl1_choice)
   }
   const play_rock = () => {
     rotateai_choice()
     decideaichoice()
+    player_choices(choices.rock_choice, ai_choice)
     play(choices.rock_choice, ai_choice)
-    call_player_choices(choices.rock_choice, ai_choice)
+    humandisplayrock()
+    displaychoicepicAI()
+    console.log(ai_choice)
   }
   const play_scissors = () => {
     rotateai_choice()
     decideaichoice()
-    call_player_choices(choices.scissors_choice, ai_choice)
+    player_choices(choices.scissors_choice, ai_choice)
     play(choices.scissors_choice, ai_choice)
+    humandisplayscissors()
+    displaychoicepicAI()
+    console.log(ai_choice)
   }
   const play_paper = () => {
     rotateai_choice()
     decideaichoice()
-    call_player_choices(choices.paper_choice, ai_choice)
+    player_choices(choices.paper_choice, ai_choice)
     play(choices.paper_choice, ai_choice)
+    humandisplaypaper()
+    displaychoicepicAI()
+    console.log(ai_choice)
   }
   const restart = () => {
     switchinnerHTML('<p>waiting for you ;)</p>')
   }
-  rockbtn.addEventListener('click', function() {play_rock();})
-  paperbtn.addEventListener('click', function() {play_paper();})
-  scissorsbtn.addEventListener('click', function() {play_scissors();})
-  restartbtn.addEventListener('click', function() {restart();})
+rockbtn.addEventListener('click', function() {play_rock();})
+paperbtn.addEventListener('click', function() {play_paper();})
+scissorsbtn.addEventListener('click', function() {play_scissors();})
+restartbtn.addEventListener('click', function() {restart();})
